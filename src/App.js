89fb2a6e663c09b3febf58f853f6cec3d6736276
PrajwolThapa <prajwol.2005@gmail.com>
2023-04-04 22:20:42 +0000

    Update App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./styles.css";
 
+function Card(props) {
+  return <div className="Card">{props.letter}</div>;
+}
+
 export default function App() {
   return (
     <div className="App">
@@ -7,15 +11,15 @@ export default function App() {
       <h2>Click to see some magic happen!</h2>
 
       <div className="grid">
-        <div class="cards">A</div>
-        <div class="cards">B</div>
-        <div class="cards">C</div>
-        <div class="cards">D</div>
-        <div class="cards">E</div>
-        <div class="cards">F</div>
-        <div class="cards">G</div>
-        <div class="cards">H</div>
-        <div class="cards">I</div>
+        <Card letter="A" />
+        <Card letter="B" />
+        <Card letter="C" />
+        <Card letter="D" />
+        <Card letter="E" />
+        <Card letter="F" />
+        <Card letter="G" />
+        <Card letter="H" />
+        <Card letter="I" />
       </div>
     </div>
   );
