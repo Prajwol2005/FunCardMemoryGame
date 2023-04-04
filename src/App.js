ca33eee284e82ae7e31eded1b4e44c90b5fcd31b
PrajwolThapa <75750574+Prajwol2005@users.noreply.github.com>
2023-04-04 17:48:10 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Card Memory Game</h1>
+      <h2>Click to see some magic happen!</h2>
+
+      <div className="grid">
+        <div class="cards">A</div>
+        <div class="cards">B</div>
+        <div class="cards">C</div>
+        <div class="cards">D</div>
+        <div class="cards">E</div>
+        <div class="cards">F</div>
+        <div class="cards">G</div>
+        <div class="cards">H</div>
+        <div class="cards">I</div>
+      </div>
+    </div>
+  );
+}
