35e531e51b4f70e84cea493696e7f6f4d288cfb6
PrajwolThapa <prajwol.2005@gmail.com>
2023-05-02 21:55:45 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import { useState } from "react";
 
 function Card(props) {
   if (props.displayLetter) {
-    return <div className="Card" onClick={props.onClick}>{props.letter}</div>;   
+    return (
+      <div className="Card" onClick={props.onClick}>
+        {props.letter}
+      </div>
+    );
   } else {
-    return <div className="Card" onClick={props.onClick}>{}</div>; 
+    return (
+      <div className="Card" onClick={props.onClick}>
+        {}
+      </div>
+    );
   }
 }
 
 export default function App() {
   const [firstCard, setFirstCard] = useState(-1);
+  const [secondCard, setSecondCard] = useState(-1);
 
   let letters = [
     "A",
@@ -32,7 +41,16 @@ export default function App() {
     "I",
     "I",
   ];
-
+  function clickCard(index) {
+    if (firstCard === -1) {
+      setFirstCard(index);
+    } else if (secondCard === -1) {
+      setSecondCard(index);
+    } else {
+      setFirstCard(-1);
+      setSecondCard(-1);
+    }
+  }
   return (
     <div className="App">
       <h1>Card Memory Game</h1>
@@ -40,7 +58,13 @@ export default function App() {
 
       <div className="grid">
         {letters.map(function (letter, index) {
-          return <Card letter={letter} displayLetter = {firstCard === index} onClick={() => setFirstCard(index)} />;
+          return (
+            <Card
+              letter={letter}
+              displayLetter={firstCard === index || secondCard === index}
+              onClick={() => clickCard(index)}
+            />
+          );
         })}
       </div>
     </div>
